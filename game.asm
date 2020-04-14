62774c3b126bd962bd5403a098f78aa9d5a204ab
S. Otzoy <otzoy97@hotmail.com>
2020-04-14 01:31:03 -0600

    game functions chagned: - now game functions are partially independent from video functions - some video functions are outdated

--- game.asm
+++ game.asm
@@ -8,7 +8,8 @@ include string.asm
     ;--------------------------------------------------
     ; Elementos generales
     ;--------------------------------------------------
-    pointc          dw 137               ;; posición inicial 151
+    pointc          dw 137               ;; posición inicial 137 (columna)
+    vram            dw ?                 ;; almacena el offset del doble buffer para el fondo del juego
     car             db 1800 dup(0)       ;; 
     good            db 900 dup(0)        ;; 
     bad             db 900 dup(0)        ;; 
@@ -24,38 +25,52 @@ include string.asm
     actualLevel     db 0                 ;; número
     actualScore     dw 3                 ;; número
     actualTime      dw 0                 ;; número
+    actualVel       dw ?                 ;; número
 .code
 
 ;--------------------------------------------------
 initGame proc far c
-; Carga el archivo del carro
+; Carga lor modelos desde los archivos .otz
+; Reserva la memoria para el doble buffer del escenario
 ;--------------------------------------------------
     ; Carga el archivo del carro
     openFile carFN, fileHandler           ;; abre el archivo
-    jc _initGame1
+    jc _initGameFailed
     readFile fileHandler, car, 1800       ;; lee el archivo
-    jc _initGame1
+    jc _initGameFailed
     closeFile fileHandler                 ;; cierra el archivo
     ; Carga el archivo del bloque amarillo
     openFile goodFN, fileHandler          ;; abre el archivo
-    jc _initGame1
+    jc _initGameFailed
     readFile fileHandler, good, 900       ;; lee el archivo
-    jc _initGame1
+    jc _initGameFailed
     closeFile fileHandler                 ;; cierra el archivo
     ; Carga el archivo del bloque verde
     openFile badFN, fileHandler           ;; abre el archivo
-    jc _initGame1
+    jc _initGameFailed
     readFile fileHandler, bad, 900        ;; lee el archivo
-    jc _initGame1
+    jc _initGameFailed
     closeFile fileHandler                 ;; cierra el archivo
+    mov ah, 48h
+    mov bx, 2025
+    int 21h                               ;; reserva la memoria para la pista
+    jc _initGameFailed
+    mov vram, ax
+    mov ax, 13h
+    int 10h                               ;; inicia el modo video
+    mov ax, 1
+    jmp _initGame1
+    _initGameFailed:
+        mov ax, 0
     _initGame1:
+        cmp ax, 1
         ret
 initGame endp
 
 ;--------------------------------------------------
 printHeader proc far c uses eax ebx ecx edx esi edi
-; Printa el texto que indica el nombre del jugador,
-; el punte
+; Escribe una línea de texto al inicio de la pantalla
+; El largo de texto es 40 bytes
 ;--------------------------------------------------
     mov ah, 02h
     mov bh, 0
@@ -73,30 +88,75 @@ printHeader endp
 ;--------------------------------------------------
 printFrame proc far c
 ; Printa el marco del juego
+; Esta función NO utiliza el doble buffer
 ;--------------------------------------------------
+    mov ah, 0ch
+    mov al, 0fh     ;; pixel blanco
     ;; barras horizontales
-    ;; comienza en 17 * 320 + 70 = 5510
-    invoke printSquare, 0fh, 5510, 180, 3
-    ;; comienza en 180 * 320 + 70 = 57670
-    invoke printSquare, 0fh, 57670, 180, 3
-    ;; barras verticales
-    ;; comienza en 17 * 320 + 67 = 5509
-    invoke printSquare, 0fh, 5507, 3, 166
-    ;; comienza en 17 * 320 + 250 = 58490
-    invoke printSquare, 0fh, 5690, 3, 166
+    mov cx, 70  ;; columna 70
+    _printF1:
+        cmp cx, 250
+        jge _printF6  
+        mov dx, 17
+        _printF2:
+            cmp dx, 20
+            jge _printF3
+            int 10h
+            inc dx
+            jmp _printF2
+        _printF3:
+            mov dx, 180
+        _printF4:
+            cmp dx, 183
+            jge _printF5
+            int 10h
+            inc dx
+            jmp _printF4
+        _printF5:
+            inc cx
+            jmp _printF1
+    _printF6:
+        mov dx, 17  ;; fila 17
+    _printF7:
+        cmp dx, 183
+        jge printF12
+        mov cx, 67
+        _printF8:
+            cmp cx, 70
+            jge _printF9
+            int 10h
+            inc cx
+            jmp _printF8
+        _printF9:
+            mov cx, 250
+        _printF10:
+            cmp cx, 253
+            jge _printF11
+            int 10h
+            inc cx
+            jmp _printF10
+        _printF11:
+            inc dx
+            jmp _printF7
+    _printF12:
     ret
 printFrame endp
 
 ;--------------------------------------------------
-printCar proc far c
-; Printa el carro utilizando dw como posición
-; inicial
+printCar proc far c uses edi esi
+; Printa la capa del carro
+; Utiliza la variabla que almacena el modelo como doble buffer
 ;--------------------------------------------------
     ;; cuerpo del carro
-    ;; comienza en 140 * 320 + pointc
+    ;; comienza en 137 * 320 + pointc
     mov bx, 43840
     add bx, pointc
-    invoke printPicture, offset car, bx, 45, 40
+    ;; sincronizar video con la imagen de carro
+    ;; en la posición de memoria de video = bx
+    ;; la figura tiene una base de 45
+    ;; se pintará 40 posiciones
+    ;; la figura se leerá desde la posición 0
+    invoke syncBuffer, offset car, bx, 45, 40, 0
     ret
 printCar endp
 
@@ -124,12 +184,17 @@ printObs proc far c uses eax ebx ecx edx, pos : byte, bType : byte
 printObs endp
 
 ;--------------------------------------------------
-printBackground proc far c
-; Printa el interior del marco del juego
+printBackground proc far c uses edi esi
+; Pinta el interior del marco del juego
 ;--------------------------------------------------
     ;; cuadrado completo
     ;; comienza en 20 * 320 + 70 = 6470
-    invoke printSquare, 7, 6470, 180, 160
+    ;; sincronizar video con el fondo gris
+    ;; en la posición de memoria 6470
+    ;; la figura tiene una base de 180
+    ;; se pintará 160 posiciones
+    ;; la figura se leerá desde la posición 3600
+    invoke syncBuffer, vram, 6470, 180, 160, 3600
     ret
 printBackground endp
 
@@ -137,6 +202,13 @@ printBackground endp
 playGame proc far c
 ; Controla las mecánicas del juego
 ;--------------------------------------------------
+    mov al, 7                            ;; codigo asignado al color gris
+    mov dx, vram
+    mov es, dx                           ;; carga la dirección de memoria para el doble buffer
+    xor di, di
+    mov cx, 32400
+    cld                                  ;; limpia el registro de flags
+    rep stosb                            ;; pinta de gris el escenario
     ret
 playGame endp
 end
--- video.asm
+++ video.asm
@@ -56,10 +56,11 @@ videoStop proc far c uses eax
 videoStop endp
 
 ;--------------------------------------------------
-syncBuffer proc far c uses edi esi, startPos : word, base : word, heigth : word
+syncBuffer proc far c uses edi esi, videoPos : word, startPos : word, base : word, heigth : word, offPos : word
 ; STARTPOS : indica el offset de la memoria de video
 ; BASE     : largo de "línea" de información a copiar en una sola iteración
 ; HEIGTH   : número de iteraciones a realizar
+; OFFPOS   : indica el offset desde donde se deberá copiar la infomración
 ; Copia la imagen almacenada en el doble buffer a la memoria de video
 ;--------------------------------------------------
     local i : word 
@@ -67,14 +68,14 @@ syncBuffer proc far c uses edi esi, startPos : word, base : word, heigth : word
     push es
     push ds
     mov i, 0            ;; inicializa i = 0
-    mov ds, vram        ;; indica la pos de mem
-    xor si, si
+    mov ds, videoPos    ;; indica la pos de mem
+    mov si, offPos
     mov dx, 0A000h
     mov es, dx          ;; indica la pos de mem de video
     _syncBuff1:
         mov bx, i
         cmp bx, heigth
-        jge 
+        jge _syncBuff2
         mov ax, 140h    ;; 320
         xor dx, dx
         mul bx          ;; 320 * i
